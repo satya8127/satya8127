329424fc452037634c0e93a3105b3e0c4266ef10
satyakumartattukolla <satyakumar.tattukolla@nagarro.com>
2022-11-29 09:58:41 +0530

    update

--- src/main/java/com/satya/employee/controller/HomeController.java
+++ src/main/java/com/satya/employee/controller/HomeController.java
@@ -6,9 +6,12 @@ import com.satya.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
@@ -30,6 +33,11 @@ public class HomeController
    @PostMapping("/add")
    public Employeedb addemployee(@RequestBody Employeedb employeedb){
       return employeeRepo.save(employeedb);
+   }
+   @GetMapping("/find")
+
+   public List<Employeedb> findemployee(){
+      return  employeeRepo.findAll();
    }
 
 }
